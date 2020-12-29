656942969e23bcdb90720b7fb602f0ad7a343122
Joanna Wytrzes <joannawytrzes@gmail.com>
2020-12-29 15:48:00 +0100

    Improve social media icons

--- src/components/molecules/SocialMedia/SocialMedia.js
+++ src/components/molecules/SocialMedia/SocialMedia.js
@@ -1,17 +1,24 @@
 import React, { forwardRef } from 'react';
 import SocialLink from '../../atoms/SocialLink/SocialLink';
-import { StyledUl, StyledIcon } from './styles-SocialMedia';
+import { StyledUl, StyledIcon, StyledIconStroke } from './styles-SocialMedia';
 import { ReactComponent as GithubIcon } from '../../../images/github.svg';
 import { ReactComponent as LinkedinIcon } from '../../../images/linkedin.svg';
+import { ReactComponent as DribbbleIcon } from '../../../images/dribbble.svg';
 
 const SocialMedia = forwardRef((props, ref) => {
 	return (
 		<StyledUl {...props} ref={ref}>
-			<SocialLink link="https://www.linkedin.com/in/joanna-wytrz%C4%99%C5%9B-45379a178/">
-				<StyledIcon as={LinkedinIcon} />
-			</SocialLink>
-			<SocialLink link="https://github.com/jWytrzes/">
+			<SocialLink link="https://github.com/jWytrzes/" label="GitHub">
 				<StyledIcon as={GithubIcon} />
+			</SocialLink>
+			<SocialLink link="https://dribbble.com/asiozaur_" label="Dribbble">
+				<StyledIconStroke as={DribbbleIcon} />
+			</SocialLink>
+			<SocialLink
+				link="https://www.linkedin.com/in/joanna-wytrz%C4%99%C5%9B-45379a178/"
+				label="Linkedin"
+			>
+				<StyledIcon as={LinkedinIcon} />
 			</SocialLink>
 		</StyledUl>
 	);
